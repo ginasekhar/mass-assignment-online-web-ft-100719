afde4c85c64319c109348442203e783ab88b9086
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-22 18:09:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
+  
+  attr_accessor :name, :user_name, :age, :location, :bio
+ 
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+  
+  
   #your code here
 end
